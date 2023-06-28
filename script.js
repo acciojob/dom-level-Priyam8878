f478505945a3fd81c3ff4a0d803011e8e887967e
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-28 12:37:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const lt = document.getElementsByTagName("li");
+//console.log(lt[2])
+for(let i=0 ;i<lt.length;i++){
+    if(lt[i].id == "level"){
+        alert("The level of the element is: "+i);
+        break;
+    }
+}
